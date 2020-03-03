ab844a8781f58d20db7e31be63b28e031b53f375
Yurii <yurii.smadych@onix-systems.com>
2020-03-03 17:36:48 +0200

    Initial commit

---
+++ src/components/Authorization.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    h1 {{message}}
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Authorization extends Vue {
+    message: string = 'Войдите в систему'
+}
+</script>
+
+<style lang="sass">
+
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    Authorization
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Authorization from './Authorization.vue';
+
+@Component({
+  components: {
+    Authorization,
+  }
+})
+export default class HelloWorld extends Vue {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
