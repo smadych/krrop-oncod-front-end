b015e968e8a73858ff5d02b1411404cb72f432a1
yurii <smadich@gmail.com>
2020-03-03 19:02:44 +0200

    Add styles

--- src/components/Authorization.vue
+++ src/components/Authorization.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
 .wrapper
     .form
-        img(src='../assets/images/logo.svg')
+        img.logo(src='../assets/images/logo.svg')
         h1 {{message}}
-        input
-        input
-        button
-        a
+        .wrapper-input
+            input(placeholder='Логин')
+            input(placeholder='Пароль')
+        .wrapper-buttons
+            button Войти
+            a Забыли пароль?
 </template>
 
 <script lang="ts">
@@ -20,6 +22,37 @@ export default class Authorization extends Vue {
 
 <style lang="scss">
 * {
-    border: 1px solid black;
+    // border: 1px solid black;
+    color: black;
 }
+html, 
+body {
+    min-height: 100%;
+}
+.wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+.form {
+  margin-top: 200px;
+  height: 444px;
+  width: 320px;
+  .logo, h1 {
+    margin-bottom: 50px;
+  }
+  .wrapper-input {
+      input {
+        border: 1px solid #D6D6E0;
+        height: 36px;
+        width: 100%;
+        border-radius: 4px;
+      }
+  }
+  .wrapper-buttons {
+      display: flex;
+      flex-direction: column;
+  }
+}
+}
+
 </style>
